5584a6502830e0a227d0e4a927f5744c0307a3df
Taildroid <131835594+Taildroid@users.noreply.github.com>
2023-04-25 22:32:42 -0500

    Update kshop.sql

--- e-commerce-admin/public/kshop.sql
+++ e-commerce-admin/public/kshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2023 at 02:57 PM
+-- Generation Time: Abr 25, 2023 at 02:57 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
